cb41b91f8301f3f4a2ebe6a2d8fe9821b921790e
Lilian Li <lil712@hotmail.com>
2022-11-16 16:24:22 +0800

    styling changes to edit button

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -4,7 +4,9 @@ const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
-      <p>{smoothie.method}</p>
+      <div className="method">
+        <p>{smoothie.method}</p>
+      </div>
       <div className="rating">{smoothie.rating}</div>
       <div className="buttons">
         <Link to={'/' + smoothie.id}>
