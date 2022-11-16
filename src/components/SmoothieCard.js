251cefc1ebfda3788a72c990dd4ea3625c1c4026
Lilian Li <lil712@hotmail.com>
2022-11-16 13:35:18 +0800

    added useParams for id in update page and useEffect, useState from supabase for data

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,4 +1,5 @@
 import { Link } from 'react-router-dom'
+
 const SmoothieCard = ({ smoothie }) => {
   return (
     <div className="smoothie-card">
@@ -6,8 +7,9 @@ const SmoothieCard = ({ smoothie }) => {
       <p>{smoothie.method}</p>
       <div className="rating">{smoothie.rating}</div>
       <div className="buttons">
-        <Link to={'/' + smoothie.id}></Link>
+        <Link to={'/' + smoothie.id}>
           <i className="material-icons">edit</i>
+        </Link>
       </div>
     </div>
   )
--- src/pages/Update.js
+++ src/pages/Update.js
@@ -1,7 +1,39 @@
+import { useParams, useNavigate } from 'react-router-dom'
+import supabase from "../config/supabaseClient"
+import { useEffect, useState } from "react"
+
 const Update = () => {
+  const { id } = useParams()
+  const navigate = useNavigate()
+
+  const [title, setTitle] = useState('')
+  const [method, setMethod] = useState('')
+  const [rating, setRating] = useState('')
+
+  useEffect(() => {
+    const fetchSmoothie = async () => {
+      const { data, error } = await supabase
+      .from('smoothies')
+      .select()
+      .eq('id', id)
+      .single()
+
+      if (error) {
+        navigate('/', { replace: true })
+      }
+      if (data) {
+        setTitle (data.title)
+        setMethod (data.method)
+        setRating(data.rating)
+        console.log(data)
+      }
+    }
+    fetchSmoothie()
+  }, [id, navigate])
+
   return (
     <div className="page update">
-      <h2>Update</h2>
+      <h2>Update - {id}</h2>
     </div>
   )
 }
