b84f85b8c2c656bfff9da5fc87293c9d94264143
Lilian Li <lil712@hotmail.com>
2022-11-17 16:37:58 +0800

    created form for login on auth page

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,26 +1,54 @@
 import supabase from "../config/supabaseClient"
+import { useState } from "react"
 
 const Auth = () => {
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('')
+  const [formError, setformError] = useState(null)
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    if (!email || !password ) {
+      setformError('Please fill in all the fields')
+      return
+    }
+  }
+
   return (
     <div className="page auth">
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor="email">Email:</label>
         <input
         type="text"
         id="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
         />
 
         <label htmlFor="password">Password:</label>
         <input
         type="text"
         id="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
         />
 
         <button>Login</button>
+        <button>Sign Up</button>
+
+        { formError && <p className="error">{formError}</p>}
 
       </form>
     </div>
     )
+}
+
+const login = (email, password) => {
+
+}
+
+const signUp = () => {
+
 }
 
 export default Auth
