d810019d6dc7bd48f9e2516c6a1929d422b60a32
Lilian Li <lil712@hotmail.com>
2022-11-17 10:54:28 +0800

    handleDelete function created

--- src/components/SmoothieCard.js
+++ src/components/SmoothieCard.js
@@ -1,6 +1,27 @@
 import { Link } from 'react-router-dom'
+import supabase from "../config/supabaseClient"
 
 const SmoothieCard = ({ smoothie }) => {
+
+  const handleDelete = async () => {
+    const { data, error } = await supabase
+      // from which table in supabase - smoothies
+      .from('smoothies')
+      .delete()
+      // delete which smoothie - the id associated with the smoothie
+      .eq('id', smoothie.id)
+      // need .select for version 2 of supabase
+      .select()
+
+      if (error) {
+        console.log(error)
+      }
+
+      if (data) {
+        console.log(data)
+      }
+  }
+
   return (
     <div className="smoothie-card">
       <h3>{smoothie.title}</h3>
@@ -12,6 +33,7 @@ const SmoothieCard = ({ smoothie }) => {
         <Link to={'/' + smoothie.id}>
           <i className="material-icons">edit</i>
         </Link>
+          <i className="material-icons" onClick={handleDelete}>delete</i>
       </div>
     </div>
   )
