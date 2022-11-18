a687a9723a2a198f159953561d4396b170e36872
Lilian Li <lil712@hotmail.com>
2022-11-18 11:23:07 +0800

    created login and signup functions

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,24 +1,47 @@
 import supabase from "../config/supabaseClient"
 import { useState } from "react"
+import { useNavigate } from "react-router-dom"
 
 const Auth = () => {
+  const navigate = useNavigate()
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
-  const [formError, setformError] = useState(null)
+  // const [formError, setformError] = useState(null)
 
-  const handleSubmit = async (e) => {
-    e.preventDefault()
-    if (!email || !password ) {
-      setformError('Please fill in all the fields')
-      return
+  const login = async (email, password) => {
+
+    try {
+      const {error} = await supabase.auth.signIn(email, password)
+      if(error) throw error
+      alert("Logged in!")
+      navigate('/')
     }
+    catch(error){
+      alert(error.message)
+    }
+
+  }
+
+  const signUp = async (email, password) => {
+
+    try {
+      const {error} = await supabase.auth.signUp(email, password)
+      if(error) throw error
+      alert("Logged in!")
+      navigate('/')
+    }
+    catch(error){
+      alert(error.message)
+    }
+
   }
 
   return (
     <div className="page auth">
-      <form onSubmit={handleSubmit}>
+      <form>
         <label htmlFor="email">Email:</label>
         <input
+        placeholder="Email"
         type="text"
         id="email"
         value={email}
@@ -27,28 +50,21 @@ const Auth = () => {
 
         <label htmlFor="password">Password:</label>
         <input
+        placeholder="Password"
         type="text"
         id="password"
         value={password}
         onChange={(e) => setPassword(e.target.value)}
         />
 
-        <button>Login</button>
-        <button>Sign Up</button>
+        <button onClick={() => login(email, password)}>Login</button>
+        <button onClick={() => signUp(email, password)}>Sign Up</button>
 
-        { formError && <p className="error">{formError}</p>}
+        {/* { formError && <p className="error">{formError}</p>} */}
 
       </form>
     </div>
     )
-}
-
-const login = (email, password) => {
-
-}
-
-const signUp = () => {
-
 }
 
 export default Auth
