fa47914bed9aeb0e9007e1099467b8eb535055bc
Lilian Li <lil712@hotmail.com>
2022-11-23 13:18:43 +0800

    added authentication login

--- src/pages/Auth.js
+++ src/pages/Auth.js
@@ -1,2 +1,45 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { supabase } from '../config/supabaseClient'
+
+const AuthContext = React.createContext()
+
+const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState()
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    // Check active sessions and sets the user
+    const session = supabase.auth.session()
+
+    setUser(session?.user ?? null)
+    setLoading(false)
+
+    // Listen for changes on auth state (logged in, signed out etc)
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      async (e, session) => {
+        setUser(session?.user ?? null)
+        setLoading(false)
+      }
+    )
+
+    return () => {
+      listener?.unsubscribe()
+    }
+  }, [])
+
+  // Will be passed down to Signup, Login and Profile
+  const value = {
+    signUp: (data) => supabase.auth.signUp(data),
+    signIn: (data) => supabase.auth.signIn(data),
+    signOut: () => supabase.auth.signOut(),
+    user,
+  }
+
+  return (
+    <AuthContext.Provider value={value}>
+      {!loading && children}
+    </AuthContext.Provider>
+  )
+}
+
+export default AuthProvider
