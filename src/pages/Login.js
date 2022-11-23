1bb1a734fc773455c93aad6683767a7bca1adcec
Lilian Li <lil712@hotmail.com>
2022-11-23 16:04:00 +0800

    added authentication logic to profile

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,7 +1,7 @@
 import { useRef, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 
-import { useAuth } from '../pages/Auth'
+import { useAuth } from './Auth'
 
 const Login = () => {
   const emailRef = useRef()
--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,15 +1,26 @@
-import { createClient } from '@supabase/supabase-js'
+import { useNavigate } from "react-router-dom"
+import { useAuth } from "./Auth"
 
 const Profile = () => {
 
+  // Get current user and signOut function from auth
+  const { user, signOut } = useAuth()
+
+  const navigate = useNavigate()
+
   const handleSignOut = async (e) => {
 
+    // Ends user session
+    await signOut()
+
+    // Redirects the user to Login page
+    navigate('/')
   }
 
 
   return (
     <div>
-      <p>Welcome!</p>
+      <p>Welcome, {user?.id}!</p>
       <button onClick={handleSignOut}>Sign out</button>
     </div>
   )
